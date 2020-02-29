be07582161023d46b49f0fef1cf070366ded851b
Jason Smith <me@iamjsmith.com>
2020-02-28 20:59:16 -0500

    Add styled components and Page, ResetCSS component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Page } from '../ui/Page';
 
 const Index = function() {
-  return (<h1>welcome home</h1>);
+  return (
+    <Page>
+      <h1>welcome home</h1>
+    </Page>
+    );
 }
 
 export default Index;

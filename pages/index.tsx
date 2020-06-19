e4b08567c8c51edd0a585bcb96e32eab1649b212
Jason Smith <me@iamjsmith.com>
2020-06-19 18:26:47 -0400

    Bring back Type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Section } from '../ui/Section';
 import { Page } from '../ui/Page';
+import { Type } from '../ui/Type';
 
 const Index: React.FC = () => {
   return (
     <Page>
-      <Section p={1}>json.nyc</Section>
+      <Section p={1}>
+        <Type>json.nyc</Type>
+      </Section>
     </Page>
   );
 };

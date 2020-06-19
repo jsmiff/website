ba0133349f6ffdece2f050e506f926cfbb5c7160
Jason Smith <me@iamjsmith.com>
2020-06-19 18:11:50 -0400

    Remove typeography component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Typography } from '../ui/Typography';
 import { Section } from '../ui/Section';
 import { Page } from '../ui/Page';
 
 const Index: React.FC = () => {
   return (
     <Page>
-      <Section p={1}>
-        <Typography as="h1">json.nyc</Typography>
-        <br />
-        <Typography as="p">bill likes to floss</Typography>
-      </Section>
+      <Section p={1}>json.nyc</Section>
     </Page>
   );
 };
